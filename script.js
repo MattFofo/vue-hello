7b0cb122b77d4d84455b4d35110e5c42d422b94e
MattFofo <matteo.segattini2@gmail.com>
2022-03-10 16:14:03 +0100

    stampato messaggio ed immagine

---
+++ script.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World, Vue is great!',
+        image: 'https://picsum.photos/500/500',
+        altImage: 'prima immagine con Vue'
+    }
+});
